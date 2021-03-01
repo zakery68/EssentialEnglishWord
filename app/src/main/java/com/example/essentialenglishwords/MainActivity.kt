378b519dfb55755e1337bfa38564a6b0a015231e
Novin-Star <zhosein068@gmail.com>
2021-03-01 13:07:49 +0330

    make picture in unit page

--- app/src/main/java/com/example/essentialenglishwords/MainActivity.kt
+++ app/src/main/java/com/example/essentialenglishwords/MainActivity.kt
@@ -1,25 +1,29 @@
 package com.example.essentialenglishwords
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.essentialenglishwords.Json.JsonProcess
 import com.example.essentialenglishwords.Process.DataProcess
+import com.example.essentialenglishwords.RecyclerView.Adapter.TransferData
 import com.example.essentialenglishwords.RecyclerView.Adapter.UnitAdapter
 import com.example.essentialenglishwords.databinding.ActivityMainBinding
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),TransferData {
 
     val jsonProcess: JsonProcess = JsonProcess()
     val dataProcess:DataProcess=DataProcess(this@MainActivity)
-    val adapter: UnitAdapter = UnitAdapter(this@MainActivity)
+    val adapter: UnitAdapter = UnitAdapter(this@MainActivity,this@MainActivity)
 
     lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
+
+
 
 
         dataProcess.save(this,adapter)
@@ -29,6 +33,13 @@ class MainActivity : AppCompatActivity() {
         mainBinding.recyclerView.adapter=adapter
 
 
+    }
+
+    override fun notifyDataForTransfer(position: Int) {
+
+        val intent=Intent(this@MainActivity,UnitPage::class.java)
+        intent.putExtra("key",position)
+        startActivity(intent)
     }
 
 }
--- app/src/main/java/com/example/essentialenglishwords/RecyclerView/Holder/UnitHolder.kt
+++ app/src/main/java/com/example/essentialenglishwords/RecyclerView/Holder/UnitHolder.kt
@@ -5,6 +5,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.unit_holder.view.*
 import kotlinx.android.synthetic.main.word_holder.view.*
 
 class UnitHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -15,5 +16,5 @@ class UnitHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     val textUnit: TextView = itemView.textUnit
 
-    val rootItem: ConstraintLayout = itemView.rootViewItem
+    val rootItem: ConstraintLayout = itemView.rootItemView
 }
